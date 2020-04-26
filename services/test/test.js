d78174f988d425abf02f45cd0dda465bbdc90b97
NicKAnderson038 <andersonnick038@gmail.com>
2020-04-26 14:33:22 -0400

    .

--- services/test/test.js
+++ services/test/test.js
@@ -1,13 +1,16 @@
 'use strict'
 
+const randomBytes = require('crypto').randomBytes
+
 module.exports.test = async event => {
 	return {
 		statusCode: 200,
 		body: JSON.stringify(
 			{
 				message: 'successfully!',
-				input: event,
+				// input: event,
 				db: process.env.USER_INFO_DB,
+				uuid: randomBytes(16).toString('hex'),
 			},
 			null,
 			2

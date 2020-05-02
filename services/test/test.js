7a558cd8884331d07b5e6fed66a7306d26604293
NicKAnderson038 <andersonnick038@gmail.com>
2020-05-02 19:27:58 -0400

    🔥 improved functionality

--- services/test/test.js
+++ services/test/test.js
@@ -3,7 +3,9 @@
 const randomBytes = require('crypto').randomBytes
 
 function getDomainName(hostName) {
-	return hostName.split('.')[0].replace('https://', '')
+	const host = hostName.split('.')[0].replace('https://', '')
+	const removePort = h => h.slice(0, h.indexOf(':'))
+	return !!(host.indexOf(':') + 1) ? removePort(host) : host
 }
 
 module.exports.test = async event => {
@@ -12,7 +14,7 @@ module.exports.test = async event => {
 		body: JSON.stringify(
 			{
 				message: 'successfully!',
-				// input: event,
+				// input: event.headers,
 				host: getDomainName(event.headers.Host),
 				db: process.env.USER_INFO_DB,
 				uuid: randomBytes(16).toString('hex'),
